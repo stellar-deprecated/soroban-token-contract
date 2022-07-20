5689dd8d1b76efe3f33839cc56ddb9d37f99ae58
Jonathan Jove <jon@stellar.org>
2022-07-20 15:58:51 -0500

    Bump dependencies and simplify register_test_contract

--- src/contract.rs
+++ src/contract.rs
@@ -43,7 +43,7 @@ pub trait TokenTrait {
 
 pub struct Token;
 
-#[contractimpl(feature = "export")]
+#[contractimpl(export_if = "export", tests_if = "external")]
 impl TokenTrait for Token {
     fn initialize(e: Env, admin: Identifier) {
         if has_administrator(&e) {
