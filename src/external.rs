e5314b579ac949ee98ce34cea0620ebb9587d97b
Siddharth Suresh <siddharth@stellar.org>
2022-07-25 09:31:23 -0700

    bump sdk (#47)

--- src/external.rs
+++ src/external.rs
@@ -5,7 +5,7 @@ use std::vec::Vec;
 use ed25519_dalek::{Keypair, Signer};
 use num_bigint::BigInt;
 use stellar_contract_sdk::xdr::{HostFunction, ScMap, ScMapEntry, ScObject, ScVal, WriteXdr};
-use stellar_contract_sdk::{Binary, Env, VariableLengthBinary};
+use stellar_contract_sdk::{Binary, Env};
 
 pub type U256 = [u8; 32];
 pub type U512 = [u8; 64];
