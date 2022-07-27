e0a784310675780f0507d5f14b60867b42861f80
Jonathan Jove <jon@stellar.org>
2022-07-27 14:39:27 -0500

    Format

--- src/nonce.rs
+++ src/nonce.rs
@@ -14,6 +14,7 @@ pub fn read_nonce(e: &Env, id: Identifier) -> BigInt {
 pub fn read_and_increment_nonce(e: &Env, id: Identifier) -> BigInt {
     let key = DataKey::Nonce(id.clone());
     let nonce = read_nonce(e, id);
-    e.contract_data().set(key, nonce.clone() + BigInt::from_u32(e, 1));
+    e.contract_data()
+        .set(key, nonce.clone() + BigInt::from_u32(e, 1));
     nonce
 }
